f3ffafc67406040ff16888bc3b0388a4eedbdb19
Douae sb <douae.sb411@gmail.com>
2025-02-25 09:06:45 +0000

    add-config-file-to-gateway-service

---
+++ gateway-service/src/main/java/com/service/gateway/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.service.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            String origin = exchange.getRequest().getHeaders().getOrigin();
+            if ("http://localhost:3000".equals(origin)) {
+                exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+                exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+                exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+                exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+                exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+
+                // Handle preflight (OPTIONS) requests
+                if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+                    exchange.getResponse().setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(exchange);
+        };
+    }
+}
